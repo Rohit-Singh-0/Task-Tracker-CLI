ce1a4d65146d12fe3bca8f314856b3ea90aa715c
Rohit-Singh-0 <rohitsingh3640@gmail.com>
2024-08-29 21:18:47 +0530

    created add task and update task methods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 #This is the Task Tracker CLI
 
 '''
@@ -21,3 +22,34 @@ Do not use any external libraries or frameworks to build this project.
 Ensure to handle errors and edge cases gracefully.
 '''
 
+#Creating a class Task Tracker
+class TaskTracker:
+    #creating the add task function to add a new task
+    def add_task(self,task_id,task,task_details,task_date, tasks):
+        self.details = {
+            task_id:{
+                "Task":task,
+                "Task Details":task_details,
+                "Task Deadline":task_date
+            }
+        }
+        return json.dumps(self.details)
+
+    def update_task(self, task_id,upd_task, upd_task_details, upd_task_date, tasks):
+        if task_id in tasks:
+            if upd_task:
+                tasks[task_id]["Task"] = upd_task
+            if upd_task_details:
+                tasks[task_id]["Task Details"] = upd_task_details
+            if upd_task_date:
+                tasks[task_id]["Task Deadline"] = upd_task_date
+
+            return tasks
+        else:
+            return "Task ID does not exists."
+
+tasks = open("tasks_record.txt", 'w')
+
+manager = TaskTracker()
+p = manager.add_task(task_id='1',task="Study", task_details="Complete Unit 1 of CN", task_date="29 August 2024", tasks=tasks)
+print(p)
